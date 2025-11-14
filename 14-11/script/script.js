8a277f5b7086ba1bd7831e47e87aaf1e7f382048
giovannilerusce7-sudo <giovannilerusce7@gmail.com>
2025-11-14 17:02:03 +0100

    script

---
+++ 14-11/script/script.js
@@ -0,0 +1,7 @@
+//let name = prompt("Inserisci il tuo nome:");
+//console.log("Nome inserito:", name);
+//alert("Ciao " + name + "! Benvenuto nel sito.");
+function saluta() {
+  let nome = document.getElementById("nome").value;
+  alert("Ciao " + nome + "!");
+}
