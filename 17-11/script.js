d79a22cd1bc8845bb96566e672a8d52804a3f55c
giovannilerusce7-sudo <giovannilerusce7@gmail.com>
2025-11-17 09:28:59 +0100

    index più script

---
+++ 17-11/script.js
@@ -0,0 +1,14 @@
+let giorno = "martedi";
+switch (giorno) {
+  case "lunedi":
+    console.log("odio il lunedi");
+    break;
+  case "martedi":
+    console.log(" è solo martedi");
+    break;
+  case "venerdi":
+    console.log(" è quasi il week-end");
+    break;
+  default:
+    console.log(" giorno non riconosciuto");
+}
